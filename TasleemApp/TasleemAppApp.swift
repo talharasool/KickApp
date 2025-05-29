c6fdffb216a456821df1a0eceda92e2a15e244bc
youthtlha <talharasool700@gmail.com>
2025-05-29 12:16:37 +0500

    Initial Commit

---
+++ TasleemApp/TasleemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasleemAppApp.swift
+//  TasleemApp
+//
+//  Created by Talha Rasool on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TasleemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
